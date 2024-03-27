4bf186fca72cc5be2bcdeedb4922bd28ef3e2153
VincentLee363787493 <vincent@coinfloww.com>
2024-03-27 18:03:25 +0800

    update main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"BlogCMS/model"
 	"BlogCMS/utils"
 	"database/sql"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
@@ -13,10 +15,12 @@ import (
 func main() {
 	r := gin.Default()
 	r.MaxMultipartMemory = 8 << 20
+	r.Use(CORSMiddleware())
 	// 处理文件上传
 	r.POST("/upload", func(c *gin.Context) {
-		file, err := c.FormFile("photo")
+		file, err := c.FormFile("file")
 		if err != nil {
+			fmt.Println("err:", err.Error())
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
@@ -25,12 +29,14 @@ func main() {
 		fileData, err := file.Open()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			fmt.Println("err:", err.Error())
 			return
 		}
 		defer fileData.Close()
 		bytes, err := ioutil.ReadAll(fileData)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			fmt.Println("err:", err.Error())
 			return
 		}
 
@@ -43,11 +49,12 @@ func main() {
 		// 保存到数据库
 		if err := model.InsertImage(utils.GetMysqlDB(), &image); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			fmt.Println("err:", err.Error())
 			return
 		}
-
-		c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully", "image_id": image.ImgUuid})
-
+		fmt.Println("ImgUuid:", image.ImgUuid)
+		//c.JSON(http.StatusOK, gin.H{"success": true, "url": image.ImgUuid})
+		c.JSON(http.StatusOK, gin.H{"location": fmt.Sprintf("http://localhost:8080/photo/%s", image.ImgUuid)})
 	})
 	r.GET("/photo/:id", func(c *gin.Context) {
 		// 从请求中获取图片ID
@@ -57,7 +64,7 @@ func main() {
 
 		photo, err := model.FindImageByName(utils.GetMysqlDB(), photoID)
 		if err != nil {
-			if err == sql.ErrNoRows {
+			if errors.Is(err, sql.ErrNoRows) {
 				// 没有找到图片
 				c.String(http.StatusNotFound, "Photo not found")
 				return
@@ -73,4 +80,19 @@ func main() {
 	})
 
 	r.Run(":8080")
+}
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
 }
